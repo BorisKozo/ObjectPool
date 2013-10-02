a78b7ab7dc3edc4b80c4d27d64dff2f231619645
Boris Kozorovitzky <majorzbzzn@gmail.com>
2013-10-02 23:20:40 +0300

    Refactoring and more options

--- test/object-pool.spec.js
+++ test/object-pool.spec.js
@@ -3,9 +3,20 @@ var op = require('./../app/object-pool');
 
 describe('Object Pool', function () {
   it('should create a new constructor token', function () {
-    should.exist(op.generate());
-    });
+    var generator = op.generate(function () { });
+    should.exist(generator);
+    should.exist(generator.constructor);
+  });
+
+  it('should create a new constructor token', function () {
+    var generator = op.generate({});
+    should.exist(generator);
+    should.exist(generator.item);
+  });
+
+
 });
+
 
 describe('Pool', function () {
 
@@ -28,6 +39,27 @@ describe('Pool', function () {
     item.should.equal(item2);
   });
 
+  it('should offload 2 out of 3 items', function () {
+    var pool = new op.$Pool();
+    var item = {};
+    pool.release(item);
+    pool.release(item);
+    pool.release(item);
+    pool.offload(2);
+    pool.count().should.equal(1);
+  });
+
+  it('should offload 3 out of 2 items', function () {
+    var pool = new op.$Pool();
+    var item = {};
+    pool.release(item);
+    pool.release(item);
+    pool.offload(3);
+    pool.count().should.equal(0);
+  });
+
+
+
   it('should be empty if we got all the items', function () {
     var pool = new op.$Pool();
     var item = {};
@@ -41,27 +73,88 @@ describe('Pool', function () {
 
 });
 
-describe('ConstructorGenerator', function () {
+describe('Constructor Generator', function () {
   var generator;
   function Point() {
     this.x = 0;
     this.y = 0;
   }
 
-  function InitPoint(x, y) {
+  function initPoint(x, y) {
     this.x = x;
     this.y = y;
   }
 
+  function destructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+
   beforeEach(function () {
-    generator = op.generate(Point, { count: 10, init: InitPoint });
+    generator = op.generate(Point, { count: 10, init: initPoint, clear:destructor });
   });
 
   it('Generator should exist', function () {
     should.exist(generator);
+    should.exist(generator.constructor);
   });
 
   it('Generator should contain 10 points', function () {
+    generator.count().should.equal(10);
+  });
+
+  it('Generator should release a point', function () {
+    var point = generator.get(1, 2);
+    point.x.should.equal(1);
+    point.y.should.equal(2);
+    generator.count().should.equal(9);
+    generator.release(point);
+    point.x.should.equal(0);
+    point.y.should.equal(0);
+    generator.count().should.equal(10);
+  });
+
+
+});
+
+describe('Clone Generator', function () {
+  var generator;
+  function clone(point) {
+    return { x: point.x, y: point.y };
+  }
+
+  function initPoint(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  function destructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+
+  beforeEach(function () {
+    generator = op.generate({ x: 0, y: 0 }, { count: 10, init: initPoint, clear: destructor, clone:clone });
+  });
+
+  it('Generator should exist', function () {
+    should.exist(generator);
+    should.exist(generator.item);
+
+  });
+
+  it('Generator should contain 10 points', function () {
+    generator.count().should.equal(10);
+  });
+
+  it('Generator should release a point', function () {
+    var point = generator.get(1, 2);
+    point.x.should.equal(1);
+    point.y.should.equal(2);
+    generator.count().should.equal(9);
+    generator.release(point);
+    point.x.should.equal(0);
+    point.y.should.equal(0);
     generator.count().should.equal(10);
   });
 
