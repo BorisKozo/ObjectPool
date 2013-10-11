e2c6e3fc77178be46820ef63fc2c0a78de561ba3
Boris Kozorovitzky <majorzbzzn@gmail.com>
2013-10-11 11:05:55 +0300

    Added Grunt task and travis integration

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+module.exports = function (grunt) {
+
+
+  grunt.initConfig({
+    uglify: {
+      options: {
+        mangle: false
+      },
+      all: {
+        files: {
+          'app/object-pool.min.js': ['app/object-pool.js'],
+          'example/object-pool.min.js': ['app/object-pool.js']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('build', ['uglify']);
+
+};
